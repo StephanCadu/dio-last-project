f367258156866598377716c63a7804b417520afc
Cadu <cdauk@hotmail.com>
2023-10-21 14:01:40 -0300

    initial commit

---
+++ src/main/java/com/last/dioproject/DioProjectApplication.java
@@ -0,0 +1,13 @@
+package com.last.dioproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioProjectApplication.class, args);
+	}
+
+}
